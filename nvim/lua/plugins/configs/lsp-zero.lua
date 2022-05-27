f4dbfbf6d77059b4c6403e451b33077524ee7dca
Villads Nielsen <villads@cozero.io>
2022-05-27 13:58:10 +0200

    fix null-ls formatting q after upgrade to 0.8

--- nvim/lua/plugins/configs/lsp-zero.lua
+++ nvim/lua/plugins/configs/lsp-zero.lua
@@ -44,6 +44,15 @@ lsp.configure("jsonls", {
 	on_attach = function(client, bufnr)
 		client.resolved_capabilities.document_formatting = false
 		client.resolved_capabilities.document_range_formatting = false
+		client.server_capabilities.documentFormattingProvider = false
+	end,
+})
+
+lsp.configure("sumneko_lua", {
+	on_attach = function(client, bufnr)
+		client.resolved_capabilities.document_formatting = false
+		client.resolved_capabilities.document_range_formatting = false
+		client.server_capabilities.documentFormattingProvider = false
 	end,
 })
 
@@ -51,6 +60,7 @@ lsp.configure("prismals", {
 	on_attach = function(client, bufnr)
 		client.resolved_capabilities.document_formatting = false
 		client.resolved_capabilities.document_range_formatting = false
+		client.server_capabilities.documentFormattingProvider = false
 	end,
 })
 
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -2,198 +2,198 @@
 local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-	packer_bootstrap = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
+  packer_bootstrap = fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
 end
 
 local packer = require("packer")
 local use = packer.use
 
 local config = function(name)
-	return string.format("require('plugins.configs.%s')", name)
+  return string.format("require('plugins.configs.%s')", name)
 end
 
 return require("packer").startup(function()
-	-- Packer can manage itself
-	use({ "wbthomason/packer.nvim" })
+  -- Packer can manage itself
+  use({ "wbthomason/packer.nvim" })
 
-	-- Colorschemes
-	use({ "navarasu/onedark.nvim" })
+  -- Colorschemes
+  use({ "navarasu/onedark.nvim" })
 
-	use({ "Shatur/neovim-ayu" })
+  use({ "Shatur/neovim-ayu" })
 
-	use({ "cormacrelf/dark-notify", config = config("dark-notify") })
+  use({ "cormacrelf/dark-notify", config = config("dark-notify") })
 
-	-- Dashboard
-	use({
-		"goolord/alpha-nvim",
-		after = "nvim-web-devicons",
-		config = config("alpha"),
-	})
+  -- Dashboard
+  use({
+    "goolord/alpha-nvim",
+    after = "nvim-web-devicons",
+    config = config("alpha"),
+  })
 
-	-- Statusline
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-		config = config("lualine"),
-		cond = function()
-			return not vim.g.vscode
-		end,
-	})
+  -- Statusline
+  use({
+    "nvim-lualine/lualine.nvim",
+    requires = { "kyazdani42/nvim-web-devicons", opt = true },
+    config = config("lualine"),
+    cond = function()
+      return not vim.g.vscode
+    end,
+  })
 
-	-- Modes
-	use({
-		"mvllow/modes.nvim",
-		config = function()
-			vim.opt.cursorline = true
-			require("modes").setup()
-		end,
-		cond = function()
-			return not vim.g.vscode
-		end,
-	})
+  -- Modes
+  use({
+    "mvllow/modes.nvim",
+    config = function()
+      vim.opt.cursorline = true
+      require("modes").setup()
+    end,
+    cond = function()
+      return not vim.g.vscode
+    end,
+  })
 
-	-- Filetree
-	use({
-		"kyazdani42/nvim-tree.lua",
-		requires = {
-			"kyazdani42/nvim-web-devicons", -- optional, for file icon
-		},
-		config = config("nvim-tree"),
-		cond = function()
-			return not vim.g.vscode
-		end,
-	})
+  -- Filetree
+  use({
+    "kyazdani42/nvim-tree.lua",
+    requires = {
+      "kyazdani42/nvim-web-devicons", -- optional, for file icon
+    },
+    config = config("nvim-tree"),
+    cond = function()
+      return not vim.g.vscode
+    end,
+  })
 
-	-- Monitor startup time
-	use({ "lewis6991/impatient.nvim" })
+  -- Monitor startup time
+  use({ "lewis6991/impatient.nvim" })
 
-	-- Boost startup time
-	use({
-		"nathom/filetype.nvim",
-		config = function()
-			vim.g.did_load_filetypes = 1
-		end,
-	})
+  -- Boost startup time
+  use({
+    "nathom/filetype.nvim",
+    config = function()
+      vim.g.did_load_filetypes = 1
+    end,
+  })
 
-	-- Tpope essentials
-	use("tpope/vim-surround")
-	use("tpope/vim-commentary")
-	use("tpope/vim-fugitive")
+  -- Tpope essentials
+  use("tpope/vim-surround")
+  use("tpope/vim-commentary")
+  use("tpope/vim-fugitive")
 
-	-- Lightspeed
-	use({ "ggandor/lightspeed.nvim", config = config("lightspeed") })
+  -- Lightspeed
+  use({ "ggandor/lightspeed.nvim", config = config("lightspeed") })
 
-	-- Telescope
-	use({
-		"nvim-telescope/telescope.nvim",
-		requires = {
-			"nvim-lua/plenary.nvim",
-		},
-		config = config("telescope"),
-	})
+  -- Telescope
+  use({
+    "nvim-telescope/telescope.nvim",
+    requires = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = config("telescope"),
+  })
 
-	use({
-		"nvim-telescope/telescope-fzf-native.nvim",
-		after = "telescope.nvim",
-		run = "make",
-		config = function()
-			require("telescope").load_extension("fzf")
-		end,
-	})
+  use({
+    "nvim-telescope/telescope-fzf-native.nvim",
+    after = "telescope.nvim",
+    run = "make",
+    config = function()
+      require("telescope").load_extension("fzf")
+    end,
+  })
 
-	-- Bufferline
-	use({ "akinsho/bufferline.nvim", after = "nvim-web-devicons", config = config("bufferline") })
+  -- Bufferline
+  use({ "akinsho/bufferline.nvim", after = "nvim-web-devicons", config = config("bufferline") })
 
-	-- Buffkill
-	use({ "qpkorr/vim-bufkill", config = config("buffkill") })
+  -- Buffkill
+  use({ "qpkorr/vim-bufkill", config = config("buffkill") })
 
-	-- Buffkill
-	use("moll/vim-bbye")
+  -- Buffkill
+  use("moll/vim-bbye")
 
-	-- Better escape
-	use({ "max397574/better-escape.nvim", event = "InsertCharPre" })
+  -- Better escape
+  use({ "max397574/better-escape.nvim", event = "InsertCharPre" })
 
-	-- Autopairs
-	use({ "windwp/nvim-autopairs", config = config("autopairs"), event = "InsertEnter" })
+  -- Autopairs
+  use({ "windwp/nvim-autopairs", config = config("autopairs"), event = "InsertEnter" })
 
-	-- Auto close tags
-	use({ "windwp/nvim-ts-autotag" })
+  -- Auto close tags
+  use({ "windwp/nvim-ts-autotag" })
 
-	-- Indentline
-	use({ "lukas-reineke/indent-blankline.nvim", config = config("indentline"), event = "BufRead" })
+  -- Indentline
+  use({ "lukas-reineke/indent-blankline.nvim", config = config("indentline"), event = "BufRead" })
 
-	-- Gitsigns
-	use({ "lewis6991/gitsigns.nvim", requires = {
-		"nvim-lua/plenary.nvim",
-	}, config = config("gitsigns") })
+  -- Gitsigns
+  use({ "lewis6991/gitsigns.nvim", requires = {
+    "nvim-lua/plenary.nvim",
+  }, config = config("gitsigns") })
 
-	-- Copilot
-	use({ "github/copilot.vim", config = config("copilot") })
+  -- Copilot
+  use({ "github/copilot.vim", config = config("copilot") })
 
-	-- TreeSitter
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		event = { "BufRead", "BufNewFile" },
-		config = config("treesitter"),
-		run = ":TSUpdate",
-	})
+  -- TreeSitter
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufRead", "BufNewFile" },
+    config = config("treesitter"),
+    run = ":TSUpdate",
+  })
 
-	--Documentation generator
-	use({
-		"danymat/neogen",
-		config = config("neogen"),
-		requires = "nvim-treesitter/nvim-treesitter",
-	})
+  --Documentation generator
+  use({
+    "danymat/neogen",
+    config = config("neogen"),
+    requires = "nvim-treesitter/nvim-treesitter",
+  })
 
-	-- Formatting
-	use({
-		"jose-elias-alvarez/null-ls.nvim",
-		requires = "PlatyPew/format-installer.nvim",
-		after = "nvim-lspconfig", -- To prevent null-ls from failing to read buffer
-		config = config("null-ls"),
-	})
+  -- Formatting
+  use({
+    "jose-elias-alvarez/null-ls.nvim",
+    requires = "PlatyPew/format-installer.nvim",
+    after = "nvim-lspconfig", -- To prevent null-ls from failing to read buffer
+    config = config("null-ls"),
+  })
 
-	-- LSP stuff
-	use({
-		"VonHeikemen/lsp-zero.nvim",
-		requires = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" },
-			{ "williamboman/nvim-lsp-installer" },
+  -- LSP stuff
+  use({
+    "VonHeikemen/lsp-zero.nvim",
+    requires = {
+      -- LSP Support
+      { "neovim/nvim-lspconfig" },
+      { "williamboman/nvim-lsp-installer" },
 
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" },
-			{ "hrsh7th/cmp-buffer" },
-			{ "hrsh7th/cmp-path" },
-			{ "saadparwaiz1/cmp_luasnip" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "hrsh7th/cmp-nvim-lua" },
+      -- Autocompletion
+      { "hrsh7th/nvim-cmp" },
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-path" },
+      { "saadparwaiz1/cmp_luasnip" },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-nvim-lua" },
 
-			-- Snippets
-			{ "L3MON4D3/LuaSnip" },
-			{ "rafamadriz/friendly-snippets" },
-		},
-		config = config("lsp-zero"),
-	})
+      -- Snippets
+      { "L3MON4D3/LuaSnip" },
+      { "rafamadriz/friendly-snippets" },
+    },
+    config = config("lsp-zero"),
+  })
 
-	-- LSP saga
-	use({
-		"tami5/lspsaga.nvim",
-		event = "BufRead",
-		after = "nvim-lspconfig",
-		config = config("lsp-saga"),
-	})
+  -- LSP saga
+  use({
+    "tami5/lspsaga.nvim",
+    event = "BufRead",
+    after = "nvim-lspconfig",
+    config = config("lsp-saga"),
+  })
 
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
-	if packer_bootstrap then
-		require("packer").sync()
-	end
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require("packer").sync()
+  end
 end)

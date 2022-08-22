9e22e964fcc50d4bc304fb47728c69ccc59490d8
Villads Nielsen <villads@cozero.io>
2022-08-22 15:23:56 +0200

    add hop config

--- nvim/lua/plugins/configs/hop.lua
+++ nvim/lua/plugins/configs/hop.lua
@@ -1,26 +1,9 @@
-require("hop").setup()
+local hop = require("hop")
+local map = require("utils").map
 
-vim.api.nvim_set_keymap(
-	"",
-	"f",
-	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>",
-	{}
-)
-vim.api.nvim_set_keymap(
-	"",
-	"F",
-	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>",
-	{}
-)
-vim.api.nvim_set_keymap(
-	"",
-	"t",
-	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>",
-	{}
-)
-vim.api.nvim_set_keymap(
-	"",
-	"T",
-	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>",
-	{}
-)
+hop.setup()
+
+map("n", "t", ":HopAnywhere<CR>")
+map("n", "T", ":HopChar1<CR>")
+map("n", "l", ":HopLineStart<CR>")
+map("n", "L", ":HopLine<CR>")

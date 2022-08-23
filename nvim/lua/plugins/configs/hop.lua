15e8a891a6177d02fd3a0aa42c29d24f5366138b
Villads Nielsen <villads@cozero.io>
2022-08-23 15:33:26 +0200

    feat(hop): add key mappings to hop set up

--- nvim/lua/plugins/configs/hop.lua
+++ nvim/lua/plugins/configs/hop.lua
@@ -3,7 +3,8 @@ local map = require("utils").map
 
 hop.setup()
 
-map("n", "t", ":HopAnywhere<CR>")
-map("n", "T", ":HopChar1<CR>")
+map("n", "t", ":HopChar1<CR>")
+map("n", "tt", ":HopChar2<CR>")
+map("n", "T", ":HopAnywhere<CR>")
 map("n", "l", ":HopLineStart<CR>")
 map("n", "L", ":HopLine<CR>")

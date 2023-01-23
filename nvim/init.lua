306734b472e530f01bae860598e28b62e0d75c86
Villads Nielsen <villads@cozero.io>
2023-01-23 11:02:43 +0100

    remove old nvim

--- nvim/init.lua
+++
@@ -1,9 +0,0 @@
-local present, impatient = pcall(require, "impatient")
-if present then
-	impatient.enable_profile()
-end
-
--- Settings
-require("settings")
-require("mappings")
-require("plugins")

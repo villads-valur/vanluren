c35922f5fe5bbea9c8a34216fc9db8b6b931a2da
Villads Nielsen <villads@cozero.io>
2023-01-23 11:09:35 +0100

    add first packages

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,4 +1,8 @@
-local plugins = {}
+local plugins = {
+ "folke/which-key.nvim",
+  { "folke/neoconf.nvim", cmd = "Neoconf" },
+  "folke/neodev.nvim",
+}
 local opts = {}
 
 require("lazy").setup(plugins, opts)

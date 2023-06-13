783127b4de4db309fee6d04deb1faa2be26e08ab
Villads Nielsen <villads@cozero.io>
2023-06-13 13:33:34 +0200

    chore: remove unused colorschemes and add key mappings to git-conflict plugin

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -21,7 +21,6 @@ require("lazy").setup({
     lazy = false,
     version = false, -- always use the latest git commit
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
--- nvim/lua/plugins/git-conflict.lua
+++ nvim/lua/plugins/git-conflict.lua
@@ -1,7 +1,21 @@
 return {
   "akinsho/git-conflict.nvim",
-  version = "*",
+  version = "v1.1.2",
+  keys = {
+    { "<leader>gml", "<cmd>GitConflictListQf<cr>", desc = "List conflicts" },
+    { "<leader>gmo", "<cmd>GitConflictChooseOurs<cr>", desc = "Merge OUR change" },
+    { "<leader>gmt", "<cmd>GitConflictChooseTheirs<cr>", desc = "Merge THEIR changes" },
+    { "<leader>gmt", "<cmd>GitConflictChooseBoth<cr>", desc = "Merge BOTH changes" },
+  },
   config = function()
-    require("git-conflict").setup()
+    require("git-conflict").setup({
+      default_mappings = false, -- disable buffer local mapping created by this plugin
+      default_commands = true, -- disable commands created by this plugin
+      disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
+      highlights = { -- They must have background color, otherwise the default color will be used
+        incoming = "DiffAdd",
+        current = "DiffText",
+      },
+    })
   end,
 }

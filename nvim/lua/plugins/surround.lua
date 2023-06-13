c9268d518a89148aa8457ee97ff99789c7e88e0f
Villads Nielsen <villads@cozero.io>
2023-06-13 13:58:09 +0200

    refactor(plugins/surround.lua): simplify configuration options

--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -1,14 +1,1 @@
-return {
-  "echasnovski/mini.surround",
-  opts = {
-    mappings = {
-      add = "S",
-      delete = "gsd",
-      find = "gsf",
-      find_left = "gsF",
-      highlight = "gsh",
-      replace = "gsr",
-      update_n_lines = "gsn",
-    },
-  },
-}
+return { "echasnovski/mini.surround" }

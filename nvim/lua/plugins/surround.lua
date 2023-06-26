eba20380fad6cd78808d9922169d78b7993c96cd
Villads Nielsen <villads@cozero.io>
2023-06-26 16:22:32 +0200

    feat: disable Leap and set custom bindings for surround

--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -1,1 +1,13 @@
-return { "echasnovski/mini.surround" }
+return {
+  "echasnovski/mini.surround",
+  opts = {
+    mappings = {
+      add = "S", -- Add surrounding in Normal and Visual modes
+      delete = "dS", -- Delete surrounding
+      find = "fS", -- Find surrounding (to the right)
+      find_left = "FS", -- Find surrounding (to the left)
+      highlight = "HS", -- Highlight surrounding
+      replace = "rS", -- Replace surrounding
+    },
+  },
+}

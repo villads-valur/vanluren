f854f1b48e7b09d06ab1edfcb3a433e90e7ecc51
Villads Nielsen <villads.nielsen@on-running.com>
2023-08-01 13:26:53 +0200

    feat: Add gitsigns.nvim plugin and integrate with null-ls.nvim

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -1,13 +1,15 @@
-return  {
-    "jose-elias-alvarez/null-ls.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = { "mason.nvim" },
-    opts = function()
-      return {
-        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-        sources = {
-          require("typescript.extensions.null-ls.code-actions")
-        },
-      }
-    end,
-  }
+return {
+  "jose-elias-alvarez/null-ls.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = { "mason.nvim" },
+  opts = function()
+    local null_ls = require("null-ls")
+    return {
+      root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+      sources = {
+        require("typescript.extensions.null-ls.code-actions"),
+        null_ls.builtins.code_actions.gitsigns,
+      },
+    }
+  end,
+}

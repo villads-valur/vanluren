9ae43db285bb9fa26a6d41bc7bb401add9b3651f
Villads Nielsen <villads.nielsen@on-running.com>
2023-10-09 11:32:25 +0200

    feat(nvim): Add terrafmt to none-ls.lua configuration

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -9,6 +9,7 @@ return {
       sources = {
         null_ls.builtins.diagnostics.rubocop,
         null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.formatting.terrafmt,
       },
     }
   end,

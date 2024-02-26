4b43ab0f818821b7ec6041263ebe15d1a7935f1a
Villads Nielsen <villads.nielsen@on-running.com>
2024-02-26 20:55:15 +0100

    refactor: Update colors in kitty theme and plugin commits

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -8,7 +8,6 @@ return {
       root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
       sources = {
         null_ls.builtins.diagnostics.rubocop,
-        null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.formatting.terrafmt,
       },
     }

c374d4e86673afadf5bc8fba7fcb7290149617c7
Villads Nielsen <villads.nielsen@on-running.com>
2024-05-02 11:00:18 +0200

    refactor(nvim): Update indent plugin and configuration in nvim lua settings

--- nvim/lua/plugins/indent.lua
+++ nvim/lua/plugins/indent.lua
@@ -1,6 +1,37 @@
 return {
-  "echasnovski/mini.indentscope",
-  opts = {
-    enable = false,
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = "LazyFile",
+    opts = {
+      indent = {
+        char = "│",
+        tab_char = "│",
+      },
+      scope = { enabled = false },
+      exclude = {
+        filetypes = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
+      },
+    },
+    main = "ibl",
+  },
+  {
+    "echasnovski/mini.indentscope",
+    opts = {
+      -- symbol = "▏",
+      symbol = "│",
+      options = { try_as_border = true },
+    },
   },
 }

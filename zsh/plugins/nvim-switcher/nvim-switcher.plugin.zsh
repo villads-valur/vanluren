75c8ddac8356dc0b989df6cebfb66d4123567226
Villads Nielsen <villads.nielsen@on-running.com>
2024-07-29 11:42:52 +0200

    feat(nvim-config): Remove some plugins and rename nvim launch alias

--- zsh/plugins/nvim-switcher/nvim-switcher.plugin.zsh
+++ zsh/plugins/nvim-switcher/nvim-switcher.plugin.zsh
@@ -1,7 +1,7 @@
-alias nvim-lazy="NVIM_APPNAME=LazyVim nvim"
+alias nvim-home="NVIM_APPNAME=HomeNvim nvim"
 
 function nvims() {
-  items=("default" "homegrown")
+  items=("default" "HomeNvim")
   config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
   if [[ -z $config ]]; then
     echo "Nothing selected"

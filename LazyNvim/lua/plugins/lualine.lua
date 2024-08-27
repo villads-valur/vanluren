0b5ec062102a904384e4592a39227f205cdc692f
Villads Nielsen <villads.nielsen@on-running.com>
2024-08-27 11:41:31 +0200

    <refactor>(lualine): Remove unnecessary functions and clean up code

--- LazyNvim/lua/plugins/lualine.lua
+++ LazyNvim/lua/plugins/lualine.lua
@@ -3,7 +3,6 @@ return {
   opts = function()
     local icons = require("lazyvim.config").icons
     local Util = require("lazyvim.util")
-
     return {
       options = {
         theme = "auto",
@@ -23,33 +22,9 @@ return {
               hint = icons.diagnostics.Hint,
             },
           },
-          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
           { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
-        -- stylua: ignore
-        {
-          function() return require("nvim-navic").get_location() end,
-          cond = function() return package.loaded["nvim-navic"] and require("nvim-navic").is_available() end,
-        },
         },
         lualine_x = {
-        -- stylua: ignore
-        {
-          function() return require("noice").api.status.command.get() end,
-          cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-          color = Util.ui.fg("Statement"),
-        },
-        -- stylua: ignore
-        {
-          function() return require("noice").api.status.mode.get() end,
-          cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-          color = Util.ui.fg("Constant"),
-        },
-        -- stylua: ignore
-        {
-          function() return "  " .. require("dap").status() end,
-          cond = function () return package.loaded["dap"] and require("dap").status() ~= "" end,
-          color = Util.ui.fg("Debug"),
-        },
           { require("lazy.status").updates, cond = require("lazy.status").has_updates, color = Util.ui.fg("Special") },
           {
             "diff",
@@ -59,10 +34,6 @@ return {
               removed = icons.git.removed,
             },
           },
-        },
-        lualine_y = {
-          { "progress", separator = " ", padding = { left = 1, right = 0 } },
-          { "location", padding = { left = 0, right = 1 } },
         },
         lualine_z = {
           function()

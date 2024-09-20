8fbc2fbaeb6343776824cede3022db52bc0e0173
Villads Nielsen <villads.nielsen@on-running.com>
2024-09-20 10:56:08 +0200

    chore(LazyNvim): Update commit hash for various plugins and add Rust language support

--- LazyNvim/lua/config/lazy.lua
+++ LazyNvim/lua/config/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.copilot-chat" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.vue" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "plugins" },

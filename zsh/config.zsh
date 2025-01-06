272c589d4e43b79b6df476e8fb360e3f88bea048
Privat Villads <villads-valur@Villadss-MacBook-Air.local>
2025-01-06 10:41:32 +0100

    feat: add ghostty config

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,3 +1,5 @@
+PATH=$PATH:/opt/homebrew/bin
+
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
 source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
@@ -8,7 +10,7 @@ plugins=(
   zsh-autosuggestions
 )
 
-source $HOME/Developer//personal/vanluren/zsh/plugins.zsh
+source $HOME/Developer/personal/vanluren/zsh/plugins.zsh
 source $HOME/Developer/personal/vanluren/zsh/variables.zsh
 source $HOME/Developer/personal/vanluren/zsh/alias.zsh
 source $HOME/Developer/personal/vanluren/zsh/functions.zsh
@@ -31,4 +33,7 @@ source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme
 # To customize prompt, run `p10k configure` or edit ~/Developer/personal/vanluren/zsh/.p10k.zsh.
 [[ ! -f ~/Developer/personal/vanluren/zsh/.p10k.zsh ]] || source ~/Developer/personal/vanluren/zsh/.p10k.zsh
 
+if [[ -n $GHOSTTY_RESOURCES_DIR ]]; then
+  source "$GHOSTTY_RESOURCES_DIR"/shell-integration/zsh/ghostty-integration
+fi
 
--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -10,10 +10,10 @@ fi
 
 load-nvmrc() {
 if [[ -f .nvmrc && -r .nvmrc ]]; then
-nvm use
+  nvm use
 elif [[ $(nvm version) != $(nvm version default)  ]]; then
 echo "Reverting to nvm default version"
-nvm use default
+  nvm use default
 fi
 }
 

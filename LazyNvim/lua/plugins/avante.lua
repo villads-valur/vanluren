c0c99ba7a209b7b0c5503a767b917ddebc5d167a
Villads Valur <villads@kvalifik.dk>
2025-03-25 11:12:55 +0100

    feat: Add functions for prefilling edit window and optimizing code

--- LazyNvim/lua/plugins/avante.lua
+++ LazyNvim/lua/plugins/avante.lua
@@ -1,3 +1,48 @@
+-- prefil edit window with common scenarios to avoid repeating query and submit immediately
+local prefill_edit_window = function(request)
+  require("avante.api").edit()
+  local code_bufnr = vim.api.nvim_get_current_buf()
+  local code_winid = vim.api.nvim_get_current_win()
+  if code_bufnr == nil or code_winid == nil then
+    return
+  end
+  vim.api.nvim_buf_set_lines(code_bufnr, 0, -1, false, { request })
+  -- Optionally set the cursor position to the end of the input
+  vim.api.nvim_win_set_cursor(code_winid, { 1, #request + 1 })
+  -- Simulate Ctrl+S keypress to submit
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-s>", true, true, true), "v", true)
+end
+
+-- NOTE: most templates are inspired from ChatGPT.nvim -> chatgpt-actions.json
+local avante_grammar_correction = "Correct the text to standard English, but keep any code blocks inside intact."
+local avante_keywords = "Extract the main keywords from the following text"
+local avante_code_readability_optimization = [[
+
+  Optimize the following code
+  You must optimize any readability issues in the code snippet.
+  Some readability issues to consider:
+  - Unclear naming
+  - Unclear purpose
+  - Redundant or obvious comments
+  - Lack of comments
+  - Long or complex one liners
+  - Too much nesting
+  - Long variable names
+  - Inconsistent naming and code style.
+  - Code repetition
+  - Inefficient code
+  - Unnecessary code
+  You may identify additional problems. The user submits a small section of code from a larger file.
+  Only list lines with readability issues, in the format <line_num>|<issue and proposed solution>
+  If there's no issues with code respond with only: <OK>
+]]
+local avante_optimize_code = "Optimize the following code"
+local avante_explain_code = "Explain the following code"
+local avante_complete_code = "Complete the following codes written in " .. vim.bo.filetype
+local avante_add_docstring = "Add docstring to the following codes"
+local avante_fix_bugs = "Fix the bugs inside the following codes if any"
+local avante_add_tests = "Implement tests for the following code"
+
 ---@class avante.CoreConfig: avante.Config
 return {
   "yetone/avante.nvim",
@@ -23,29 +68,44 @@ return {
     copilot = {
       endpoint = "https://api.githubcopilot.com",
       model = "gpt-4o-2024-08-06",
-      proxy = nil, -- [protocol://]host[:port] Use this proxy
       allow_insecure = false, -- Allow insecure server connections
       timeout = 30000, -- Timeout in milliseconds
       temperature = 0,
       max_tokens = 4096,
     },
-    ---@class AvanteHintsConfig
-    ---@field enabled boolean
+    dual_boost = {
+      enabled = true,
+      first_provider = "openai",
+      second_provider = "copilot",
+      prompt = "Based on the two reference outputs below, generate a response that incorporates elements from both but reflects your own judgment and unique perspective. Do not provide any explanation, just give the response directly. Reference Output 1: [{{provider1_output}}], Reference Output 2: [{{provider2_output}}]",
+      timeout = 60000, -- Timeout in milliseconds
+    },
+    behavior = {
+      auto_suggestions = true,
+      support_paste_from_clipboard = true,
+    },
     hints = {
       enabled = true,
     },
-    --- @class AvanteConflictUserConfig
     diff = {
       autojump = true,
       list_opener = "copen",
     },
     windows = {
       wrap = true, -- similar to vim.o.wrap
-      width = 25, -- default % based on available width
+      width = 30, -- default % based on available width
       sidebar_header = {
         align = "left", -- left, center, right for title
         rounded = false,
       },
+      ask = {
+        floating = false, -- Open the 'AvanteAsk' prompt in a floating window
+        start_insert = true, -- Start insert mode when opening the ask window
+        focus_on_apply = "ours", -- which diff to focus after applying
+      },
     },
+  },
+  keys = {
+    { mode = { "v" } },
   },
 }

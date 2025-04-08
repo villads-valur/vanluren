a5ac8be5bd913d43ec6632b34c34ca3c63e03e84
Villads Valur <villads@kvalifik.dk>
2025-04-08 16:34:12 +0200

    feat: Add LazyGit key binding for Git actions

--- LazyNvim/lua/plugins/snacks.lua
+++ LazyNvim/lua/plugins/snacks.lua
@@ -12,10 +12,10 @@ return {
 			╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝ ╚═════╝        ╚═╝
         ]],
         -- stylua: ignore
-        ---@type snacks.dashboard.Item[]
         keys = {
           { icon = " ", key = "f", desc = "Find file", action = "<CMD>Telescope find_files<CR>" },
           { icon = " ", key = "r", desc = "Recent files", action = "<CMD>Telescope oldfiles<CR>" },
+          { icon = "", key = "g", desc = "LazyGit", action = function() LazyVim.lazygit( { cwd = LazyVim.root.git() }) end},
           { icon = " ", key = "u", desc = "Update plugins", action = "<CMD>Lazy update<CR>" },
           { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
           { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },

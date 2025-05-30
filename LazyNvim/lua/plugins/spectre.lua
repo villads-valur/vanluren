843da671422d0f85ad716670f964347201a2b973
Villads Valur <villads@kvalifik.dk>
2025-05-30 14:40:05 +0200

    feat: Add key mappings for search and replace in Spectre

--- LazyNvim/lua/plugins/spectre.lua
+++ LazyNvim/lua/plugins/spectre.lua
@@ -1,3 +1,26 @@
 return {
   "nvim-pack/nvim-spectre",
+  keys = {
+    {
+      "<leader>sr",
+      function()
+        require("spectre").open()
+      end,
+      desc = "Search and Replace",
+    },
+    {
+      "<leader>sR",
+      function()
+        require("spectre").open_visual({ select_word = true })
+      end,
+      desc = "Search and Replace (Visual)",
+    },
+    {
+      "<leader>sw",
+      function()
+        require("spectre").open_visual({ select_word = false })
+      end,
+      desc = "Search and Replace (Visual Word)",
+    },
+  },
 }

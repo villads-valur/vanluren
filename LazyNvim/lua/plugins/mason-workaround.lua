8fc7a47baf734f6c228f43731560a4c6d735891e
Villads Valur <villads@kvalifik.dk>
2025-11-05 11:07:14 +0100

    refactor: Remove copilot.lua and update mason-workaround.lua

--- LazyNvim/lua/plugins/mason-workaround.lua
+++ LazyNvim/lua/plugins/mason-workaround.lua
@@ -1,3 +1,31 @@
 return {
-  { "mason-org/mason.nvim", version = "^1.0.0" },
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      registries = {
+        "github:mason-org/mason-registry",
+      },
+    },
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    config = function()
+      -- Patch registry.refresh to handle nil updated_registries
+      local registry = require("mason-registry")
+      local original_refresh = registry.refresh
+
+      registry.refresh = function(callback)
+        return original_refresh(function(success, updated_registries)
+          -- Ensure updated_registries is never nil
+          callback(success, updated_registries or {})
+        end)
+      end
+
+      require("mason-lspconfig").setup({
+        ensure_installed = {},
+        automatic_installation = false,
+        automatic_enable = false,
+      })
+    end,
+  },
 }

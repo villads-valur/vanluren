1ba06354ae64a863516476057104c970ace16085
Villads Valur <villads-valur@Villadss-MacBook-Pro.local>
2025-12-09 17:52:43 +0100

    chore: remove old nvim file, rename

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,44 +1,62 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-	},
-	keys = function()
-		return {
-			{
-				"<leader>fe",
-				function()
-					require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
-				end,
-				desc = "Explorer NeoTree (cwd)",
-			},
-			{ "<leader>n", "<leader>fE", desc = "Explorer NeoTree", remap = true },
-			{ "<leader>N", "<leader>fe", desc = "Explorer NeoTree (root)", remap = true },
-		}
-	end,
-	opts = {
-		filesystem = {
-			bind_to_cwd = true,
-			follow_current_file = {
-				enabled = false,
-			},
-			use_libuv_file_watcher = true,
-		},
-		window = {
-			mappings = {
-				["<space>"] = "none",
-			},
-			position = "right",
-		},
-		default_component_configs = {
-			indent = {
-				with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
-				expander_collapsed = "",
-				expander_expanded = "",
-				expander_highlight = "NeoTreeExpander",
-			},
-		},
-	},
+  "nvim-neo-tree/neo-tree.nvim",
+  keys = function()
+    return {
+      {
+        "<leader>n",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+        end,
+        desc = "Explorer NeoTree",
+        remap = true,
+      },
+      {
+        "<leader>N",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = require("lazyvim.util").root.get() })
+        end,
+        desc = "Explorer NeoTree (root)",
+        remap = true,
+      },
+    }
+  end,
+  opts = {
+    filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = {
+        enabled = true,
+      },
+      use_libuv_file_watcher = true,
+      filtered_items = {
+        visible = false, -- when true, they will just be displayed differently than normal items
+        hide_dotfiles = true,
+        always_show_by_pattern = { -- uses glob style patterns
+          ".github*",
+          ".gitignore",
+        },
+      },
+    },
+    window = {
+      mappings = {
+        ["<space>"] = "none",
+      },
+      position = "right",
+    },
+    default_component_configs = {
+      indent = {
+        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+        expander_collapsed = "",
+        expander_expanded = "",
+        expander_highlight = "NeoTreeExpander",
+      },
+    },
+    event_handlers = {
+      {
+        event = "file_open_requested",
+        handler = function()
+          require("neo-tree.command").execute({ action = "close" })
+        end,
+      },
+    },
+  },
 }
--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,30 +1,21 @@
 return {
-	{
-		"0xstepit/flow.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-		config = function()
-			require("flow").setup({
-				transparent = false, -- Set transparent background.
-				fluo_color = "pink", --  Fluo color: pink, yellow, orange, or green.
-				mode = "normal", -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
-				aggressive_spell = false, -- Display colors for spell check.
-			})
-
-			vim.cmd("colorscheme flow")
-		end,
-	},
-	{
-		"Shatur/neovim-ayu",
-		priority = 1000,
-	},
-	{
-		"Vanluren/neovim-onehalf",
-		priority = 1000,
-		lazy = false,
-		config = function()
-			vim.cmd("colorscheme onehalf")
-		end,
-	},
+  {
+    "0xstepit/flow.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      require("flow").setup({
+        transparent = false, -- Set transparent background.
+        fluo_color = "orange", --  Fluo color: pink, yellow, orange, or green.
+        mode = "normal", -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
+        aggressive_spell = false, -- Display colors for spell check.
+      })
+    end,
+  },
+  {
+    "Shatur/neovim-ayu",
+    lazy = true,
+    priority = 1000,
+  },
 }

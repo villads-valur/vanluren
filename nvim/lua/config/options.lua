084d82ff09fee5f085eccb3e2224601e74f23b85
Villads Valur <villads.valur@legora.com>
2025-12-12 11:02:27 +0100

    feat(lazy.lua): add biome formatter plugin for enhanced formatting support feat(options.lua): set prettier to require config file for formatting

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,6 +3,7 @@ local opt = vim.opt
 opt.conceallevel = 0 -- dont' conceal "" and ''
 opt.swapfile = false -- don't use swapfile
 vim.g.lazyvim_eslint_auto_format = true
+vim.g.lazyvim_prettier_needs_config = true
 
 vim.api.nvim_create_user_command("SortJsonKeys", function()
   if vim.bo.filetype ~= "json" then

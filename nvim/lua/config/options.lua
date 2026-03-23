8fa2ee2f1996b4a0eb4a20bb9d0a076a0abb2feb
Villads Valur <villads.valur@legora.com>
2026-03-23 08:30:41 +0100

    fix(nvim): typescript not working for monorepo

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,8 +2,12 @@ local opt = vim.opt
 
 opt.conceallevel = 0 -- dont' conceal "" and ''
 opt.swapfile = false -- don't use swapfile
-vim.g.lazyvim_eslint_auto_format = true
+vim.g.lazyvim_eslint_auto_format = false
 vim.g.lazyvim_prettier_needs_config = true
+
+local npmCacheDir = vim.fn.stdpath("data") .. "/npm-cache"
+vim.fn.mkdir(npmCacheDir, "p")
+vim.env.NPM_CONFIG_CACHE = npmCacheDir
 
 vim.api.nvim_create_user_command("SortJsonKeys", function()
   if vim.bo.filetype ~= "json" then
--- nvim/lua/plugins/mason-workaround.lua
+++ nvim/lua/plugins/mason-workaround.lua
@@ -9,23 +9,23 @@ return {
   },
   {
     "mason-org/mason-lspconfig.nvim",
-    config = function()
+    opts = function(_, opts)
       -- Patch registry.refresh to handle nil updated_registries
       local registry = require("mason-registry")
-      local original_refresh = registry.refresh
+      if not registry._refresh_patch_applied then
+        local original_refresh = registry.refresh
 
-      registry.refresh = function(callback)
-        return original_refresh(function(success, updated_registries)
-          -- Ensure updated_registries is never nil
-          callback(success, updated_registries or {})
-        end)
+        registry.refresh = function(callback)
+          return original_refresh(function(success, updated_registries)
+            if callback then
+              callback(success, updated_registries or {})
+            end
+          end)
+        end
+
+        registry._refresh_patch_applied = true
       end
-
-      require("mason-lspconfig").setup({
-        ensure_installed = {},
-        automatic_installation = false,
-        automatic_enable = false,
-      })
+      return opts
     end,
   },
 }
